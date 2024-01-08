d00c3c82b0cdeefe866bd452f04476f0a220536d
YangEleven <2257754297@qq.com>
2024-01-08 17:19:46 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- 结构 -->
+  <div class="app">
+    <h1>你好</h1>
+  </div>
+</template>
+
+<script lang="ts">
+// ts / js
+export default {
+  name: "App",
+};
+</script>
+
+<style scoped>
+.app {
+  background-color: #ddd;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+// 用于创建应用
+
+import App from "./App.vue";
+
+createApp(App).mount("#app");
